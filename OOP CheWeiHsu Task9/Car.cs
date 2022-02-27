775172e0b4369c5dcbf10b6e90a679d843145118
KingOfSpicy <sangowosan@gmail.com>
2022-02-27 10:06:49 +0200

    Add the car & truck properties.

---
+++ OOP CheWeiHsu Task9/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CheWeiHsu_Task9
+{
+    //Base class:
+    class Car
+    {
+        public Car(string make, string model, int year, string color)
+        {
+            Console.WriteLine("Calling base class constructor.");
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}-{1}-{2}-{3}", Make, Model, Year, Color);
+        }
+    }
+}
---
+++ OOP CheWeiHsu Task9/Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CheWeiHsu_Task9
+{
+    //Derived class:
+    class Truck:Car
+    {
+        public Truck(string make, string model, int year, string color, int towingCapacity) : base(make, model, year, color)
+        {
+            Console.WriteLine("Calling derived class constructor.");
+            TowingCapacity = towingCapacity;
+        }
+
+        public int TowingCapacity { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString()+String.Format("-{0}", TowingCapacity);
+        }
+    }
+}
